6283b5d175132f97392d0071f63d2dd46a795698
Max Arzubov <m.arzubov@gmail.com>
2016-06-03 19:00:00 +0300

    Add lang query parameter to every request.

--- app/ajax/service.js
+++ app/ajax/service.js
@@ -6,16 +6,37 @@ export default AjaxService.extend({
   i18n: Ember.inject.service(),
   namespace: config.APP.API_NAMESPACE,
   host: config.APP.API_HOST,
-  headers: Ember.computed('i18n.locale', {
+  // headers: Ember.computed('i18n.locale', {
+  //   get() {
+  //     let headers = {};
+  //     const locale = this.get('i18n.locale');
+
+  //     if (locale) {
+  //       headers['Accept-Language'] = locale;
+  //     }
+
+  //     return headers;
+  //   }
+  // }),
+  data: Ember.computed('i18n.locale', {
     get() {
-      let headers = {};
+      let data = {};
       const locale = this.get('i18n.locale');
 
       if (locale) {
-        headers['Accept-Language'] = locale;
+        data['lang'] = locale;
       }
 
-      return headers;
+      return data;
     }
-  })
+  }),
+  _getFullDataHash(data) {
+    const classData = Ember.get(this, 'data') || {};
+    const _data = Ember.merge({}, classData);
+    return Ember.merge(_data, data);
+  },
+  request(url, options = {}) {
+    options.data = this._getFullDataHash(options.data);
+    return this._super(url, options);
+  }
 });
